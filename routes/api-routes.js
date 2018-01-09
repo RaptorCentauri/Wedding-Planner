b87cd905bffbf0b3653821d5a7ac4bcc63b25f84
ncloyd <ncloyd33@gmail.com>
2018-01-09 08:53:16 -0600

    some API routes done, still working on it

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,22 +1,155 @@
 //Require models from models direcrtory
+// var Venue = require('../models/venue.js');
+// var Music = require('../models/music.js');
+// var Flowers = require('../models/flowers.js');
+// var BridalShop = require('../models/bridalShops.js');
+var db = require('..models')
 
-module.exports = function(app){
-
-
+module.exports = function(app) {
 //get routes
-  app.get(`/api/:foo?`, function(req, res){
-    //code goes here
+ //venues
+  app.get(`/api/:venues?`, function(req, res){
+    if (req.params.venues) {
+        db.Venue.findOne({
+          where: {
+            routeName: req.params.characters
+          }
+        }).then(function(result){
+          return res.json(result);
+        });
+      } 
+      else {
+        db.Venue.findAll({})
+        .then(function(result) {
+          return res.json(result);
+        });
+      }
+    });
+
+  //bridalshops
+  app.get('/api/:bridalshops?', function(req, res) {
+    if (req.params.bridalshops) {
+      db.BridalShop.findOne({
+        where: {
+          routeName: req.params.bridalshops
+        }
+      }).then(function(result) {
+        return res.json(result);
+      });
+    } 
+    else {
+      db.BridalShop.findAll({})
+      .then(function(result) {
+        return res.json(result);
+      });
+    }
   });
 
-//post route
-  app.post(`/api/new`, function(req, res){
-    //code here
+  //music
+  app.get('/api/:music?', function(req, res) {
+    if (req.params.music) {
+      db.Music.findOne({})
+      .then(function(result) {
+        return res.json(result);
+      });
+    } 
+    else {
+      db.Music.findAll({})
+        .then(function(result) {
+          return res.json(result);
+        });
+    }
+  });
 
+  //flowers
+  app.get('./api/:flowers?', function(req, res) {
+    if (req.params.flowers) {
+      db.Flowers.findOne({
+       where: { 
+          routeName: req.params.flowers
+       }
+      })
+      .then(function(result) {
+        return res.json(result);
+      });
+    }
+    else {
+      db.Flowers.findAll({})
+        .then(function(result) {
+          return res.json(result);
+        });
+    }
+  });
+
+
+//post route
+  app.post(`/api/venues`, function(req, res){
+      var venues = req.body;
+      var routeName = venue.name.replace(/\s+/g, "").toLowerCase();
+
+      db.Venue.create({
+        routeName: routeName,
+        name: venue.name,
+        address: venue.address,
+        location: venue.location,
+        capacity: venue.capacity,
+        phone: venue.phone,
+        cost: venue.cost
+      });
+  });
+
+  app.post(`/api/bridalshops`, function(req, res){
+      var bridalshop = req.body;
+      var routeName = bridalshop.name.replace(/\s+/g, "").toLowerCase();
+
+     db.BridalShop.create({
+        routeName: routeName,
+        name: bridalshop.name,
+        address: bridalshop.address,
+        phone: bridalshop.phone,
+        email: bridalshop.email,
+        cost: bridalshop.cost
+      });
+  });
+
+  app.post(`/api/music`, function(req, res){
+      var music = req.body;
+      var routeName = music.name.replace(/\s+/g, "").toLowerCase();
+
+      db.Music.create({
+        routeName: routeName,
+        name: music.name,
+        phone: music.phone,
+        email: music.email,
+        cost: music.cost
+      });
+  });
+
+  app.post(`/api/flowers`, function(req, res){
+      var flower = req.body;
+      var routeName = flower.name.replace(/\s+/g, "").toLowerCase();
+
+     db.Flowers.create({
+        routeName: routeName,
+        name: flower.name,
+        phone: flower.phone,
+        email: flower.email,
+        cost: flower.cost
+      });
   });
 
   //update
-  app.put(`/api/:foo`, function(req, res){
+  app.put(`/api/:venues`, function(req, res){
+    //code here
+  });
+  app.put(`/api/:bridalshops`, function(req, res){
+    //ode here
+  });
+  app.put(`/api/:music`, function(req, res){
+    //code here
+  });
+  app.put(`/api/:flowers`, function(req, res){
     //code here
   });
 
-}
+};
