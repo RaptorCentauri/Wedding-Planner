3acb2bb1ea6fc3a2ebb5fc3eb9d59ffc69f6fcda
melissaintrees <melissaintrees@gmail.com>
2018-01-11 14:44:33 -0600

    new version of index.html and app.js with Yaeinn's and Dave's changes commented out for a short while

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,45 +1,69 @@
 // Declare an object of Dummy Data to test the JQuery With
 
-// var venues = {
-//   name: "The Hideaway",
-//   address: "555 Address Lane",
-//   phone: "(555)555-5555",
-//   cap: "300"
-// };
+var venues = {
+  name: "The Hideaway",
+  address: "555 Address Lane",
+  phone: "(555)555-5555",
+  cap: "300"
+};
+var venObject;
+
+// this function takes the data object and prints the add to planner button to every option in the object
+
+// function to populate options:
+
 
 $(document).ready(function(){
   // this is the venue button
   $("#venue-id").on("click", function(event){
-    $(".infoRow").empty();
+    $("#venues").empty();
         event.preventDefault();
 
       $.get( "/api/venues", function(data){
-      console.log("DATA: ", data);
-      console.log("this is a get request");
-      for(var i = 0; i < data.length; i ++){
-        var venName = data[i].name;
-        var venAdd = data[i].address;
-        var venLoc = data[i].location;
-        var venCap = data[i].capacity;
-        var venPhone = data[i].phone;
-        var venCost = data[i].cost;
+      // console.log("DATA: ", data);
+      // console.log("this is a get request");
+      var venResults = data;
+      // for every result in the data object do this
+      for(var i = 0; i < venResults.length; i++){
+        // for every result in the data object, create a div called venue item to be dynamically added to the page and then assign it to the venue div
+        var venueDiv = $("<div class=\"venue-item\">");
+        // adds the card title html dynam. to page
+        var cardTitle = $(`<div class="col s6 m4">
+                            <div class="card alice-blue">
+                              <div class="card-content grey-text text-darken-3">`);
+        //append card title to the the venDiv var
+        venDiv.append(cardTitle)
+        //adds the results of each venues name to a var called venName
+        var venName = venResults[i].name;
+        // makes a par to display that var
+        var p = $("<p class=\"card-title\">").text("Name: " + venName);
+        // appends the par var to the venDiv var
+        venueDiv.append(p);
+        // appends everything to the venues id
+        $("#venues").append(venueDiv);
+        // Dave's changes commented out
+        // $(".infoRow").append(
+        //           `<div class="col s6 m4">
+        //           <div class="card alice-blue">
+        //             <div class="card-content grey-text text-darken-3">
+        //               <span class="card-title venue-name">${venObject.venName}</span>
+        //               <p class="title venue-address">${venObject.venAdd}</p>
+        //               <p class="title venue-phone">${venObject.venPhone}</p>
+        //               <p class="title venue-cap">${venObject.venCap}</p>
+        //             </div>
+        //             <div class="card-action">
+        //               <button class="addVenuePlanner" data-id="${i}">Add to Planner</button>
+        //               <a href="#">Add to Planner</a>
+        //             </div>
+        //           </div>
+        //         </div>`
+        // );
+        // $(".addVenuePlanner").on("click", function(){
+        //    $(".venuePlanner").attr( "data-id", venObject.venAdd[i]);
 
-        $(".infoRow").append(
-                  `<div class="col s6 m4">
-                  <div class="card alice-blue">
-                    <div class="card-content grey-text text-darken-3">
-                      <span class="card-title venue-name">${venName}</span>
-                      <p class="title venue-address">${venAdd}</p>
-                      <p class="title venue-phone">${venPhone}</p>
-                      <p class="title venue-cap">${venCap}</p>
-                    </div>
-                    <div class="card-action">
-                      <button class="addVenuePlanner" data-id="${i}">Add to Planner</button>
-                      <a href="#">Add to Planner</a>
-                    </div>
-                  </div>
-                </div>`
-        );
+        // });
+
+
     }
     }, "json");
 
@@ -48,19 +72,19 @@ $(document).ready(function(){
 
 
 
-$(document.body).on("click", ".addVenuePlanner", function(){
-  console.log("click working");
+// $(document.body).on("click", ".addVenuePlanner", function(){
+//   console.log("click working");
 
-  // let blah = this.data("data-id");
-  $(".venuePlanner").html("test append");
+//   // let blah = this.data("data-id");
+//   $(".venuePlanner").html("test append");
 
-  console.log(this);
-  // console.log(blah);
+//   console.log(this);
+//   // console.log(blah);
 
-})
+// })
 
 
- // $('.collapsible').collapsible();
+ $('.collapsible').collapsible();
 
 
 // function recipeReturn(ID) {
@@ -74,3 +98,12 @@ $(document.body).on("click", ".addVenuePlanner", function(){
 //                 .append(`<a href=${response.attribution.url} target="_blank"><img class="recipe-image" src=${response.images[0].imageUrlsBySize["360"]} /></a>`);
 //             $("#recipeHome").append(div);
 //         });
+
+        // venObject = {
+        //   venName: venResults[i].name,
+        //   venAdd: venResults[i].address,
+        //   venLoc: venResults[i].location,
+        //   venCap: venResults[i].capacity,
+        //   venPhone: venResults[i].phone,
+        //   venCost: venResults[i].cost
+        // }
