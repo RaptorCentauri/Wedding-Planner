1f1506528d19b772109767a983e7390f706a23f4
ncloyd <ncloyd33@gmail.com>
2018-01-12 23:53:34 -0600

    added to seeds file, 6 cards across is visually appealling

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,29 @@
-INSERT INTO bridal_shops (name, address, phone, email, cost) VALUES ("Lovely Bride","3529 Sarah Lane","3125559054","appointment@lovelybride.com", 750);
-INSERT INTO bridal_shops (name, address, phone, email, cost) VALUES ("Gigi's Couture","10 Michigan Avenue","8475556743","sarah@gigiscouture.com", 5000);
-INSERT INTO bridal_shops (name, address, phone, email, cost) VALUES ("Bella Bridal","2500 W Roosevelt Boulevard","3125559438","book@bellabridal.com", 10000);
-INSERT INTO bridal_shops (name, address, phone, email, cost) VALUES ("La Petite Boutique","34 W Maple Street","3125556723","schedule@boutique.com", 750000);
-INSERT INTO bridal_shops (name, address, phone, email, cost) VALUES ("Lily Ann's Closet","319 S Le Claire Avenue","8475554285","lilyann@gmail.com", 2500);
+USE wedding_planner_db;
 
-INSERT INTO flowers (name, phone, email, cost) VALUES ("Bloom","8475558442","nicole@bloom.com", 1200);
-INSERT INTO flowers (name, phone, email, cost) VALUES ("Fleur de l'Amour","3125551550","order@fleur.com", 2500);
-INSERT INTO flowers (name, phone, email, cost) VALUES ("Bunches & Bows","3125552400","dave@bunches.com", 800);
-INSERT INTO flowers (name, phone, email, cost) VALUES ("Yaeinn's Flower Shop","8475558965","order@yaeinnsflowershop.com", 1500);
-INSERT INTO flowers (name, phone, email, cost) VALUES ("Flora","3125556421","flora@comcast.net", 5000);
+INSERT INTO bridal_shops (name, address, phone, email, cost) VALUES ("Lovely Bride","3529 Sarah Lane","312-555-9054","appointment@lovelybride.com", 1250);
+INSERT INTO bridal_shops (name, address, phone, email, cost) VALUES ("Gigi's Couture","10 Michigan Avenue","847-555-6743","sarah@gigiscouture.com", 5000);
+INSERT INTO bridal_shops (name, address, phone, email, cost) VALUES ("Bella Bridal","2500 W Roosevelt Boulevard","312-555-9438","book@bellabridal.com", 10000);
+INSERT INTO bridal_shops (name, address, phone, email, cost) VALUES ("La Petite Boutique","34 W Maple Street","312-555-6723","schedule@boutique.com", 750000);
+INSERT INTO bridal_shops (name, address, phone, email, cost) VALUES ("Lily Ann's Closet","319 S Le Claire Avenue","847-555-4285","lilyann@gmail.com", 700);
+INSERT INTO bridal_shops (name, address, phone, email, cost) VALUES ("Vivian's Bridal Boutqiue","5460 N Vera Lane","847-555-3100","book@viviansbridal.com", 2500);
 
-INSERT INTO music (name, phone, email, cost) VALUES ("Melissa and the Wedding Singers","3125553889","melissa23@gmail.com","2000");
-INSERT INTO music (name, phone, email, cost) VALUES ("Five Strings Orchestra","3125554577","fivestrings@comcast.net", 1500);
-INSERT INTO music (name, phone, email, cost) VALUES ("Shilander & Smith Band","8475552330","dave@gmail.com", 1000);
-INSERT INTO music (name, phone, email, cost) VALUES ("The Reception Crashers","3125552332","reserve@receptioncrashers.net", 1250);
-INSERT INTO music (name, phone, email, cost) VALUES ("Groove Machine","8475550921","book@groovemachine.com",900);
+INSERT INTO flowers (name, phone, email, cost) VALUES ("Bloom","847-555-8442","nicole@bloom.com", 1200);
+INSERT INTO flowers (name, phone, email, cost) VALUES ("Fleur de l'Amour","312-555-1550","order@fleur.com", 2500);
+INSERT INTO flowers (name, phone, email, cost) VALUES ("Bunches & Bows","312-555-2400","dave@bunches.com", 800);
+INSERT INTO flowers (name, phone, email, cost) VALUES ("Yaeinn's Flower Shop","847-555-8965","order@yaeinnsflowershop.com", 1500);
+INSERT INTO flowers (name, phone, email, cost) VALUES ("Flora","312-555-6421","flora@comcast.net", 8000);
+INSERT INTO flowers (name, phone, email, cost) VALUES ("Stems","312-555-6776","order@stems.com", 3200);
 
-INSERT INTO venues (name, address, location, capacity, phone, cost) VALUES ("Kearny Supper Club","7242 N Main St","outdoor", 60,"81855509878", 1300);
-INSERT INTO venues (name, address, location, capacity, phone, cost) VALUES ("The Werbowsky Lounge","35000 Sam Adams Terrace","indoor", 250,"7735556486", 4000);
-INSERT INTO venues (name, address, location, capacity, phone, cost) VALUES ("The Penthouse at The Herman ","5 E Infinity Place 85th Floor","both", 500,"6305551509", 25000);
-INSERT INTO venues (name, address, location, capacity, phone, cost) VALUES ("Storino's","1676 N Lincoln","indoor", 200,"7735559078", 8500);
-INSERT INTO venues (name, address, location, capacity, phone, cost) VALUES ("Karl Thomas' Distillery and Lodge","4242 E Barrel Junction","indoor", 150 ,"8475557424", 12000);
+INSERT INTO music (name, phone, email, cost) VALUES ("Melissa and the Wedding Singers","312-555-3889","melissa23@gmail.com",2000);
+INSERT INTO music (name, phone, email, cost) VALUES ("Five Strings Orchestra","312-555-4577","fivestrings@comcast.net", 1500);
+INSERT INTO music (name, phone, email, cost) VALUES ("Shilander & Smith Band","847-555-2330","dave@gmail.com", 1000);
+INSERT INTO music (name, phone, email, cost) VALUES ("The Reception Crashers","312-555-2332","reserve@receptioncrashers.net", 1250);
+INSERT INTO music (name, phone, email, cost) VALUES ("Groove Machine","847-555-0921","book@groovemachine.com",900);
+INSERT INTO music (name, phone, email, cost) VALUES ("Wedding Banned","312-555-3333","book@groovemachine.com",700);
+
+INSERT INTO venues (name, address, location, capacity, phone, cost) VALUES ("Kearny Supper Club","7242 N Main St","outdoor", 60,"818-555-09878", 1300);
+INSERT INTO venues (name, address, location, capacity, phone, cost) VALUES ("The Werbowsky Lounge","35000 Sam Adams Terrace","indoor", 250,"773-555-6486", 4000);
+INSERT INTO venues (name, address, location, capacity, phone, cost) VALUES ("The Penthouse at The Herman ","5 E Infinity Place 85th Floor","both", 500,"630-555-1509", 25000);
+INSERT INTO venues (name, address, location, capacity, phone, cost) VALUES ("Storino's","1676 N Lincoln","indoor", 200,"773-555-9078", 8500);
+INSERT INTO venues (name, address, location, capacity, phone, cost) VALUES ("Karl Thomas' Distillery and Lodge","4242 E Barrel Junction","indoor", 150 ,"847-555-7424", 12000);
+INSERT INTO venues (name, address, location, capacity, phone, cost) VALUES ("The Einsenberger","450 N Linden St","indoor", 80 ,"630-555-3220", 6500);
