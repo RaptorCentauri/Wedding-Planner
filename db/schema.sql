80757f8f0bcfdd373230f493e00cca14811b87be
Melissa St Moore <melissaintrees@gmail.com>
2018-07-30 21:11:50 -0500

    add connection details for jawsdb

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS wedding_planner_db;
-CREATE DATABASE wedding_planner_db;
+DROP DATABASE IF EXISTS t05i9vyhmo8m5rd9;
+CREATE DATABASE t05i9vyhmo8m5rd9;
 
-USE wedding_planner_db;
+USE t05i9vyhmo8m5rd9;
 
 CREATE TABLE bridal_shops (
 id INT auto_increment NOT NULL,
@@ -10,6 +10,7 @@ address VARCHAR(100) NOT NULL,
 phone VARCHAR(15) NOT NULL,
 email VARCHAR(100) NOT NULL,
 website VARCHAR(100) NOT NULL,
+createdAt TIMESTAMP NOT NULL,
 primary key(id)
 );
 
@@ -19,6 +20,7 @@ name VARCHAR(100) NOT NULL,
 phone VARCHAR(15) NOT NULL,
 email VARCHAR(100) NOT NULL,
 website VARCHAR(100) NOT NULL,
+createdAt TIMESTAMP NOT NULL,
 primary key(id)
 );
 
@@ -28,6 +30,7 @@ name VARCHAR(100) NOT NULL,
 phone VARCHAR(15) NOT NULL,
 email VARCHAR(100) NOT NULL,
 website VARCHAR(100) NOT NULL,
+createdAt TIMESTAMP NOT NULL,
 primary key(id)
 );
 
@@ -37,6 +40,7 @@ name VARCHAR(100) NOT NULL,
 address VARCHAR(100) NOT NULL,
 phone VARCHAR(15) NOT NULL,
 website VARCHAR(100) NOT NULL,
+createdAt TIMESTAMP NOT NULL,
 primary key(id)
 );
 
